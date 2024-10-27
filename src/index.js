1b708d9c0bdea5d19d58a8549833f68e70f861dc
Volha Yurchanka <volha@yurchanka.ru>
2024-10-27 07:47:50 +0300

    feat: add solution to this function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+  const fromNulltoNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tensToNinety = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number < 20) return fromNulltoNineteen[number];
+  if (number < 100) return tensToNinety[Math.floor(number / 10)] + (number % 10 ? ' ' + fromNulltoNineteen[number % 10] : '');
+  if (number < 1000) return fromNulltoNineteen[Math.floor(number / 100)] + ' hundred' + (number % 100 ? ' ' + toReadable(number % 100) : '');
+
+  return '';
 }
